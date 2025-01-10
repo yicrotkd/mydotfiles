e4656f8a46ab24bec93e8ec29aa0e32150bb073e
yicrotkd <yoichirotkd@gmail.com>
2025-01-10 16:23:21 +0900

    update config

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -1,10 +1,37 @@
 return {
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	{
+		"almo7aya/openingh.nvim",
+		config = function()
+			vim.api.nvim_create_user_command("GBrowse", "OpenInGHFile", {
+				register = vim.g.openingh_copy_to_register,
+				range = true,
+				bang = true,
+				nargs = "*",
+			})
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		-- load the plugin at startup
+		event = "VeryLazy",
+		-- Because of the keys part, you will be lazy loading this plugin.
+		-- The plugin wil only load once one of the keys is used.
+		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
+		-- or lazy = false. One of both options will work.
+		opts = {
+			-- your configuration comes here
+			-- for example
+			enabled = true, -- if you want to enable the plugin
+			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		},
+	},
 
 	{
 		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
 		event = "InsertEnter",
 		config = function()
 			require("copilot").setup({
